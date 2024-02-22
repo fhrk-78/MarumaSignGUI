ab5a276151be36aa66401da357031bd316b4beb6
PizzaHarumaki <78fhruby78@gmail.com>
2024-02-22 10:29:31 +0900

    仮修正

--- src/main/java/io/github/fhrk_78/msigngui/client/MarumaSignGUIClient.java
+++ src/main/java/io/github/fhrk_78/msigngui/client/MarumaSignGUIClient.java
@@ -9,6 +9,8 @@ import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,48 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MarumaSignGUIClient implements ClientModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("marumasigngui");
+    @Override
+    public void onInitializeClient() {
+        // multiSave初期化
+        multiSave[0] = "";
+        multiSave[1] = "";
+        multiSave[2] = "";
+        multiSave[3] = "";
+        LOGGER.info("Multisave Initialised");
+
+        // キーバインドの作成
+        KeyBinding nCKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.marumasigngui.newcreatekey",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_N,
+                "key.category.marumasigngui.msgct"
+        ));
+        LOGGER.info("NewCreate KeyBind Initialised");
+
+        KeyBinding cLKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.marumasigngui.copylinekey",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_B,
+                "key.category.marumasigngui.msgct"
+        ));
+        LOGGER.info("MultiLineCopy KeyBind Initialised");
+
+        // クライアントティックイベントにキーバインドの処理を登録
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (nCKeyBinding.wasPressed()) {
+                MinecraftClient.getInstance().setScreen(new CottonClientScreen(new GenGUI()));
+            }
+        });
+        LOGGER.info("NewCreate KeyBind Registered");
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (cLKeyBinding.wasPressed()) {
+                MinecraftClient.getInstance().setScreen(new CottonClientScreen(new CopyLineByLineGUI()));
+            }
+        });
+        LOGGER.info("MultiLineCopy KeyBind Registered");
+    }
+
     public static String[] multiSave = new String[4];
 
     public static List<String> utlGenLines(String tmp_s) {
@@ -31,40 +75,5 @@ public class MarumaSignGUIClient implements ClientModInitializer {
             }
         }
         return list;
-    }
-
-    @Override
-    public void onInitializeClient() {
-        // multiSave初期化
-        multiSave[0] = "";
-        multiSave[1] = "";
-        multiSave[2] = "";
-        multiSave[3] = "";
-
-        // キーバインドの作成
-        KeyBinding nCKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.marumasigngui.newcreatekey",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_N,
-                "key.category.marumasigngui.msgct"
-        ));
-        KeyBinding cLKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.marumasigngui.copylinekey",
-                InputUtil.Type.KEYSYM,
-                GLFW.GLFW_KEY_B,
-                "key.category.marumasigngui.msgct"
-        ));
-
-        // クライアントティックイベントにキーバインドの処理を登録
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (nCKeyBinding.wasPressed()) {
-                MinecraftClient.getInstance().setScreen(new CottonClientScreen(new GenGUI()));
-            }
-        });
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (cLKeyBinding.wasPressed()) {
-                MinecraftClient.getInstance().setScreen(new CottonClientScreen(new CopyLineByLineGUI()));
-            }
-        });
     }
 }
